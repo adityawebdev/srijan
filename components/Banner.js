f3ef47187c1fee8a418f5a2431b00733044bbb8a
vansh bhardwaj <bhardwajvansh26@gmail.com>
2023-03-20 21:34:04 +0530

    20-Carousel-footerText

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from 'react-bootstrap/Carousel';
 
-import { banner1, banner2, banner3 } from "../assets";
+
+// IMP --- banner3 keeps breaking, i.e. undefined
+import { banner1, banner2 } from "../assets";
 import Image from "next/image";
 
 const Banner = () => {
-  return <div>banner</div>;
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <Image
+            src={banner1}
+            className="d-block w-100"
+            style={{ objectFit: "cover" }}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            src={banner2}
+            className="d-block w-100"
+            style={{ objectFit: "cover" }}
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
