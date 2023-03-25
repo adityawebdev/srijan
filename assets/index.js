615e9749889814629f4bfbd49a9cc1380bca6795
vansh bhardwaj <bhardwajvansh26@gmail.com>
2023-03-25 15:41:10 +0530

    events and eventid.js

--- assets/index.js
+++ assets/index.js
@@ -4,10 +4,8 @@ import aditya from "./team/aditya.jpg";
 import jatin from "./team/jatin.jpg";
 import jatin2 from "./team/jatin2.jpg";
 import gola from "./team/gola.jpg";
-
 import karuna from "./team/karuna.jpg";
 import loocina from "./team/loocina.jpg";
-
 import manas from "./team/manas.jpg";
 import res from "./team/res.jpg";
 import shivaji from "./team/shivaji.jpg";
@@ -22,6 +20,25 @@ import shree from "./team/shree.jpg";
 
 import banner1 from "./banner1.jpg";
 import banner2 from "./banner2.jpg";
+
+import fashion from "./event-thumbnails/fashion.png";
+import friend from "./event-thumbnails/friend.png";
+import soloSing from "./event-thumbnails/soloSing.png";
+import BoB from "./event-thumbnails/BoB.png";
+import soloDance from "./event-thumbnails/soloDance.png";
+import groupDance from "./event-thumbnails/groupDance.png";
+import mono from "./event-thumbnails/mono.png";
+import nukkad from "./event-thumbnails/nukkad.png";
+import debate from "./event-thumbnails/debate.png";
+import interjection from "./event-thumbnails/interjection.png";
+import poetry from "./event-thumbnails/poetry.png";
+import murder from "./event-thumbnails/murder.png";
+import shortFilm from "./event-thumbnails/shortFilm.png";
+import objectPromo from "./event-thumbnails/objectPromo.png";
+import leaf from "./event-thumbnails/leaf.png";
+import musicArt from "./event-thumbnails/musicArt.png";
+import exhibition from "./event-thumbnails/exhibition.png";
+import decathlon from "./event-thumbnails/decathlon.png";
 
 export {
   aryan,
@@ -45,4 +62,22 @@ export {
   gola,
   banner1,
   banner2,
+  fashion,
+  friend,
+  soloSing,
+  BoB,
+  soloDance,
+  groupDance,
+  mono,
+  nukkad,
+  debate,
+  interjection,
+  poetry,
+  murder,
+  shortFilm,
+  objectPromo,
+  leaf,
+  musicArt,
+  exhibition,
+  decathlon
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      green: "#126656",
-      peach: "#f8f4e8",
-      orange: "#cf8e55",
-      black: "#ffffff",
-      indigo: "#5e0fff",
-      blue: "#1905d9",
-      lightpink: "#fe86ff",
-      babypink: "fd2bff",
-      deeppink: "#c202d3",
+    
+    extend: {
+      colors: {
+        green: "#126656",
+        peach: "#f8f4e8",
+        orange: "#cf8e55",
+        black: "#ffffff",
+        indigo: "#5e0fff",
+        blue: "#1905d9",
+        lightpink: "#fe86ff",
+        babypink: "fd2bff",
+        deeppink: "#c202d3",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
