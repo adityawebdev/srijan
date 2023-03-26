83645cedcfc91691d64b9aed86b9d182b563b628
Aditya Sinha <adityasinha3201@gmail.com>
2023-03-27 04:59:23 +0530

    trying to solve pulse error

--- components/Pass.js
+++ components/Pass.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Pass = () => {
   return (
-    <div className=" animate-pulse bg-black text-white p-4 font-bold text-xl text-center">
+    <div className="  bg-black text-white p-4 font-bold text-xl text-center">
       <a href="https://forms.gle/HJqTNFDSfD9WHgxv7">Get Your Concert Passes</a>
     </div>
   );
